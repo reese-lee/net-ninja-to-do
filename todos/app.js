a1711a9a41585bbcee06331d646ed6f0d7f143d3
Reese Lee <reese.lee.cy@gmail.com>
2019-11-03 22:54:58 -0800

    add code to reset form

--- todos/app.js
+++ todos/app.js
@@ -15,6 +15,10 @@ addForm.addEventListener('submit', e => {
     e.preventDefault();
     const todo = addForm.add.value.trim();
    
-    generateTemplate(todo);
+    if (todo.length) {
+        generateTemplate(todo);
+        addForm.reset();
+    }
+  
 
 });
